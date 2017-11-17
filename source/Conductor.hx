f127a55efa8e3c362e24a1450034f759309230a5
Cameron <cameron.taylor.ninja@gmail.com>
2017-11-17 11:41:19 -0800

    Day one work heheh

---
+++ source/Conductor.hx
@@ -0,0 +1,19 @@
+package;
+
+/**
+ * ...
+ * @author ninjaMuffin
+ */
+class Conductor 
+{
+	public static var bpm:Float = 67.13;
+	public static var crochet:Float = (60 / bpm) * 1000;//beats in milliseconds
+	public static var songPosition:Float;
+	
+	
+	public function new() 
+	{
+		
+	}
+	
+}
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,19 +3,115 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.system.FlxSound;
 import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
 
 class PlayState extends FlxState
 {
+	private var song:FlxSound;
+	private var lastBeat:Float;
+	private var lastBar:Float;
+	private var _totalBeats:Float = 0;
+	private var _totalBars:Float = 0;
+	
+	/**
+	 * What to multiply the crochet by to create a bar
+	 */
+	private var _barMulitplier:Float = 4;
+	
+	
+	private var _inhale:Bool = true;
+	private var _text:FlxText;
+	
+	
 	override public function create():Void
 	{
+		FlxG.fixedTimestep = false;
+		
+		initSong();
+		
+		lastBeat = 0;
+		lastBar = 0;
+		
+		_text = new FlxText(0, 0, 0, "Inhale", 30);
+		_text.screenCenter();
+		add(_text);
+		
+		textResize();
+		
 		super.create();
+	}
+	
+	
+	private function initSong():Void
+	{
+		song = new FlxSound();
+		song.loadEmbedded("assets/music/I-Am.mp3");
+		add(song);
+		song.play();
 	}
 
 	override public function update(elapsed:Float):Void
-	{
+	{	
 		super.update(elapsed);
+		
+		Conductor.songPosition = song.time;
+		
+		positionCheck();
+		
+		if (_inhale)
+		{
+			_text.text = "Inhale";
+		}
+		else
+		{
+			_text.text = "Exhale";
+		}
+		
+	}
+	
+	private function positionCheck():Void
+	{
+		//every beat
+		if (Conductor.songPosition > lastBeat + Conductor.crochet)
+		{
+			lastBeat += Conductor.crochet;
+			
+			_totalBeats += 1;
+			_totalBars = _totalBeats / 4;
+			
+			FlxG.log.add("BEEAT" + _totalBeats);
+			
+		}
+		
+		//Runs every bar
+		if (Conductor.songPosition > lastBar + (Conductor.crochet * _barMulitplier))
+		{
+			lastBar += Conductor.crochet * _barMulitplier;
+			
+			_inhale = !_inhale;
+			
+			
+			textResize();
+			
+			
+			
+			FlxG.log.add("BAR " + _totalBars);
+		}
+	}
+	
+	private function textResize():Void
+	{
+		if (_inhale)
+		{
+			FlxTween.tween(_text, {size: 50}, Conductor.crochet * _barMulitplier * 0.001);
+		}
+		else
+		{
+			FlxTween.tween(_text, {size: 30}, Conductor.crochet * _barMulitplier * 0.001);
+		}
 	}
 }
